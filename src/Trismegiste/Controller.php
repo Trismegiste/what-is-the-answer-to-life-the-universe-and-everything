826ddff7168575985626ad3374b990707417cf3f
Florent <florent.g@allopneus.com>
2013-06-27 12:30:49 +0200

    randomness

--- src/Trismegiste/Controller.php
+++ src/Trismegiste/Controller.php
@@ -8,13 +8,24 @@ namespace Trismegiste;
 
 use Silex\Application;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Finder\Finder;
 
 class Controller
 {
 
     public function home(Request $request, Application $app)
     {
-        return $app->render('home.html.twig', array());
+        $fnd = new Finder();
+        $iter = $fnd->files()
+                ->in(dirname($app['webdir']) . '/views')
+                ->path('answer')
+                ->name('*.html.twig');
+
+        $choice = iterator_to_array($iter);
+        shuffle($choice);
+        $choice = $choice[0]->getRelativePathname();
+
+        return $app->render($choice, array());
     }
 
 }
